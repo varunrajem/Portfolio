8402b7d229e6aee86cdd53b5ad41423005e189a0
Varunraje <varunraje47@gmail.com>
2023-10-04 07:38:11 +0530

    fix/card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Image from 'next/image'
+import { FaMusic } from 'react-icons/fa'
 
 
 const Card = (props) => {
   return (
-    <div className='w-64 h-[400px] shadow-md rounded-xl bg-white mr-24 cursor-pointer hover:animate-pulse'>
-      <div className=''>
-        <Image src={props.img} alt='pic' className='w-auto h-64 rounded-md object-cover ' />
+    <div className='shadow-md rounded-xl bg-white cursor-pointer group'>
+      <div className='w-full relative overflow-hidden rounded-md'>
+        <Image src={props.img} alt='pic' className='w-full h-72 rounded-md object-cover' />
+        <div className='absolute inset-0 flex justify-center items-center -translate-x-[100%] transition-all ease-in-out duration-500 group-hover:translate-x-0 group-hover:bg-black/70'>
+          <div className='p-5 flex flex-col justify-center'>
+            <FaMusic size={20} className='text-white' />
+            <p className='font-semibold text-white'>Movie Booking App</p>
+          </div>
+        </div>
       </div>
       <p className='text-center text-xl font-bold p-4'>{props.name}</p>
       <div className='flex justify-center gap-3'>
