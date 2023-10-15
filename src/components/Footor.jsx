0e57cde34e720ca511e580a6b53a68a70aa5d681
Varunraje <varunraje47@gmail.com>
2023-10-15 17:53:13 +0530

    Footor

--- src/components/Footor.jsx
+++ src/components/Footor.jsx
@@ -6,15 +6,29 @@ import { AiFillInstagram } from "react-icons/ai";
 
 const Footor = () => {
   return (
-    <div className='h-40 w-full shadow-lg bg-blue-100'>
-      <div className='flex justify-center gap-3 '>
-        <AiFillGithub size={40} className='cursor-pointer rounded-full' />
-        <FaSquareXTwitter size={40} color='black' className='cursor-pointer rounded-full ' />
-        <AiFillLinkedin size={40} color='blue' className='cursor-pointer rounded-full' />
-        <AiFillInstagram size={40} color='#e75480' className='cursor-pointer rounded-full' />
-      </div>
+    <footer className="bg-white rounded-lg shadow m-4">
+      <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+        <span className="text-3xl font-bold text-blue-700 sm:text-center">DEV-VARUN
+        </span>
+        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 sm:mt-0">
+          <li>
+            <AiFillInstagram size={36} color='#e75480' className='cursor-pointer rounded-full' />
+          </li>
+          <li>
+            <AiFillLinkedin size={36} color='blue' className='cursor-pointer rounded-full' />
 
-    </div>
+          </li>
+          <li>
+            <FaSquareXTwitter size={36} color='black' className='cursor-pointer rounded-full ' />
+
+          </li>
+          <li>
+            <AiFillGithub size={36} color='black' className='cursor-pointer rounded-full' />
+          </li>
+        </ul>
+      </div>
+    </footer>
+
   )
 }
 
